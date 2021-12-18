233072d4aef96e9a048b2566900060b9029ffd9d
Siddhartha <siddpurwar@gmail.com>
2021-12-18 19:55:50 +0530

    Implementation is working
    Need to insert functionality
    
    Files:
        modified:   .gitignore
        modified:   implementation.cpp
        modified:   implementation.h
        modified:   main.cpp
        modified:   makefile
        modified:   task

--- main.cpp
+++ main.cpp
@@ -1,33 +1,35 @@
 #include <iostream>
+#include <vector>
 #include "implementation.h"
 
 using namespace std;
 
 int main(int argc, char *argv[]) {
+    vector<string> args(argv, argv + argc);
     ToDo session;
     switch (argc) {
         case 2:
-            if (argv[1] == "help") {
+            if (args[1] == "help") {
                 cout << session.cmd_help() << endl;
             }
-            else if (argv[1] == "ls") {
+            else if (args[1] == "ls") {
                 cout << session.cmd_ls() << endl;
             }
-            else if (argv[1] == "report") {
+            else if (args[1] == "report") {
                 cout << session.cmd_report() <<endl;
             }
             break;
         case 3:
-            if (argv[1] == "del") {
-                session.cmd_delete(argv[2]); 
+            if (args[1] == "del") {
+                session.cmd_delete(args[2]); 
             }
-            else if (argv[1] == "done") {
-                session.cmd_complete(argv[2]);
+            else if (args[1] == "done") {
+                session.cmd_complete(args[2]);
             }
             break;
         case 4:
-            if (argv[1] == "add") {
-                session.cmd_add(argv[2], argv[3]);
+            if (args[1] == "add") {
+                session.cmd_add(args[2], args[3]);
             }
             break;
         default:
--- makefile
+++ makefile
@@ -1,4 +1,3 @@
 todo_start: main.cpp implementation.cpp implementation.h
 	@g++ -c implementation.cpp
 	@g++ -o task implementation.o main.cpp
-	@./task
